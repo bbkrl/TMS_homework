cc34016c88af5713ec83002477f471e900c3d028
bbkrl <bagushevich2003@mail.ru>
2022-04-03 15:39:56 +0300

    создать класс 'Wheel'

--- homework8/main.py
+++ homework8/main.py
@@ -7,25 +7,45 @@ class Engine:
     коэффициент разгона от турбины (если он есть)
 
     """
-    motor_type_v6 = 0.2
-    __motor_type_v8 = 0.4
-    __motor_type_v10 = 0.6
+    __engine_type_v6 = 0.2
+    __engine_type_v8 = 0.4
+    __engine_type_v10 = 0.6
     __turbine_coefficient = 0.8
 
-    def __init__(self, engine_type, horse_power: int, turbine=False):
-        self.engine_type = engine_type
+    def __init__(self, horse_power: int, engine_type=__engine_type_v10, turbine=False):  # TODO хз как сделать более
+        # TODO норм выбор типа мотора
+        self.__engine_type_v6 = engine_type
         self.horse_power = horse_power
         self.turbine = turbine
 
     def generate_power(self):
         if self.turbine:
-            engine_power = (self.horse_power * self.engine_type) / 0.8
+            engine_power = (self.horse_power * self.__engine_type_v6) / self.__turbine_coefficient
         else:
-            engine_power = (self.horse_power * self.engine_type)
+            engine_power = (self.horse_power * self.__engine_type_v6)
         return engine_power
 
 
-if __name__ == "__name__":
-    print(1111)
-    engine1 = Engine(0.2, 639, False)
-    print(type(engine1))
+class Wheel:
+    """
+    Wheel (колесо), который принимает в себя диаметр (16-21, необходимо сделать проверку на вхождение в этот
+    диапазон) и вес колеса в кг. Этот класс методов не имеет
+    """
+
+    def __init__(self, wheel_weight, wheel_diameter):
+        self.wheel_weight = wheel_weight
+        while True:
+            self.wheel_diameter = wheel_diameter
+            if 16 <= wheel_diameter <= 21:
+                break
+            else:
+                print("Неподходящий диметр")
+                continue
+
+
+
+
+
+
+jz_2 = Engine(1000, turbine=True)
+print(jz_2.generate_power())

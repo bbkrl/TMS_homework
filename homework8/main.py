36c27aa0c52cee830024d3d64fa96c21ebe5e6dd
bbkrl <bagushevich2003@mail.ru>
2022-04-03 20:57:48 +0300

    реализован класс Car с методом move

--- homework8/main.py
+++ homework8/main.py
@@ -7,20 +7,19 @@ class Engine:
     коэффициент разгона от турбины (если он есть)
 
     """
-    __engine_type_v6 = 0.2
-    __engine_type_v8 = 0.4
-    __engine_type_v10 = 0.6
-    __turbine_coefficient = 0.8
 
-    def __init__(self, horse_power: int, engine_type=__engine_type_v10, turbine=False):  # TODO хз как сделать более
-        # TODO норм выбор типа мотора
-        self.__engine_type_v6 = engine_type
+    def __init__(self, horse_power: int, turbine: bool):
+        # self.engine_type = engine_type
         self.horse_power = horse_power
-        self.turbine = turbine
+        self.__turbine = turbine
+        self.__engine_type_v6 = 0.2
+        self.__engine_type_v8 = 0.4
+        self.__engine_type_v10 = 0.6
+        self.__turbine_coefficient = 0.8
 
     def generate_power(self):
-        if self.turbine:
-            engine_power = (self.horse_power * self.__engine_type_v6) / self.__turbine_coefficient
+        if self.__turbine:
+            engine_power = (self.horse_power * self.__engine_type_v10) / self.__turbine_coefficient
         else:
             engine_power = (self.horse_power * self.__engine_type_v6)
         return engine_power
@@ -34,18 +33,45 @@ class Wheel:
 
     def __init__(self, wheel_weight, wheel_diameter):
         self.wheel_weight = wheel_weight
-        while True:
-            self.wheel_diameter = wheel_diameter
-            if 16 <= wheel_diameter <= 21:
-                break
-            else:
-                print("Неподходящий диметр")
-                continue
+        self.wheel_diameter = wheel_diameter
 
 
+class Car(Engine, Wheel):
+    """Car (машина), который принимает в себя объект мотора, массив из объектов колес, если передано меньше 4 –
+    выкидывать ошибку(почитайте о raise Exception) и тип машины: легковая, джип и грузовая, каждый из типов имеет свой
+    вес в кг (легковая 1200, джип 1500,  грузовая 1800) Этот класс имеет следующие методы: start_engine,
+    который заводит мотор move, который принимает в себя дистанцию на заезд в метрах и возвращает время в секундах за
+    которое эту дистанцию проедет автомобиль. Логика вычисления следующая: 1.	Если двигатель не заведен - выкидывать
+    ошибку (почитайте о raise Exception) 2.	Форму вычисления времени: (Вес автомобиля + вес колес) / мощность мотора (
+    из метода generate_power) * длинна заезда
+    """
+
+    def __init__(self, horse_power: int, turbine: bool, wheel_weight: int, wheel_diameter: int, num_of_wheel: int):
+        Engine.__init__(self, horse_power, turbine)
+        Wheel.__init__(self, wheel_weight, wheel_diameter)
+        self.num_of_wheel = num_of_wheel
+        self.__passenger = 1200
+        self.__truck = 1800
+        self.__jeep = 1500
+
+    def start_engine(self, key: int) -> bool:
+        if key:
+            engine_running = True
+            print('Двигатель запущен')
+            return engine_running
+
+        else:
+            engine_running = False
+            print('Двигатель выключен')
+            return engine_running
+
+    def move(self, distance: int) -> float:
+        if not Car.start_engine:
+            raise Exception("Двигатель не запущен!")
+        move_time = ((self.wheel_weight * self.num_of_wheel + self.__jeep) * distance) / self.generate_power()
+        return move_time
 
 
-
-
-jz_2 = Engine(1000, turbine=True)
-print(jz_2.generate_power())
+car1 = Car(639, False, 20, 19, 4)
+car1.start_engine(0)
+print(car1.move(100))
